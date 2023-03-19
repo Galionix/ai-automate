67681305c93c90c7991ec9bacbe4860d1f2d918b
dmitry <galionix2@gmail.com>
2023-03-19 17:04:00 +0200

    init app

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import { ActionsLogger } from './modules/logger';
+
+
+const actionsLogger = new ActionsLogger();
+
+
+(function() {
+    'use strict';
+
+    console.log('ai-automate.ts3');
+    actionsLogger.init();
+
+})();
